2a207c4ced6fec075dd5af9bde0052e23f61f046
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2024-11-29 17:01:48 +0100

    fix basename

--- checks/exercise-01.sh
+++ checks/exercise-01.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-awk 'BEGIN{status="UNKNOWN"} \
+awk 'function basename(file) {sub(".*/", "", file); return file }  \
+     BEGIN{status="UNKNOWN"} \
     /^s .*$/{status=$2} \
     /^.* search$/{search=$2;searchpercent=$3} \
     /^.* simplify$/{simplify=$2;simplifypercent=$3} \
     /^c total real time since initialization:/{totaltime=$7} \
-    END{printf("%s,%s,%s,%s,%s,%s,%s\n", substr(FILENAME,0,length(FILENAME)-6), status,totaltime,search,0+searchpercent,simplify,0+simplifypercent)}' $1
+    END{printf("%s,%1.2f,%1.2f,%1.2f,%1.2f,%1.2f,%1.2f\n", substr(basename(FILENAME),0,length(basename(FILENAME))-4), status,totaltime,search,0+searchpercent,simplify,0+simplifypercent)}' $1
